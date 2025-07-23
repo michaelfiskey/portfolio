f5f6f2832206e937e7c98ef074216ffed670194d
Michael Fiskey <fiskey.michael@gmail.com>
2025-07-23 13:22:18 -0500

    front page ideation

--- portfolio-project/src/app/(pages)/contact/page.tsx
+++ portfolio-project/src/app/(pages)/contact/page.tsx
@@ -13,19 +13,35 @@ const Page = () => {
                         <p>this is some more text to test out what im trying to do</p>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center mt-10 border border-gray-700 w-auto">
+                <div className="flex flex-col justify-center mt-10 border border-gray-700">
                     <div className="bg-stone-700 w-full items-center justify-center p-1">
                         <h2 className="h2 text-white text-center">Contact Forum</h2>
                     </div>
                     <p className="m-5">Feel free to contact me whatever it may be!</p>
-                    <div className="m-15 flex flex-row gap-20 justify-center items-center w-auto">
-                        <div className="flex flex-col w-full">
-                            <label className="whitespace-nowrap w-full">First Name:</label>
-                            <input className="input mt-auto" />
+                    <div className="m-15 flex flex-col justify-center gap-5">
+                        <div className="flex flex-row gap-20 justify-center items-center ">
+                            <div className="flex flex-col w-full">
+                                <label className="whitespace-nowrap w-full">First Name <sup className="text-red-500">*</sup></label>
+                                <input className="input mt-auto" />
+                            </div>
+                            <div className="flex flex-col w-full">
+                                <label className="whitespace-nowrap w-full">Last Name <sup className="text-red-500">*</sup></label>
+                                <input className="input mt-auto" />
+                            </div>
                         </div>
-                        <div className="flex flex-col w-full">
-                            <label className="whitespace-nowrap w-full">Last Name:</label>
-                            <input className="input mt-auto" />
+                        <div className="flex flex-row gap-20 justify-center items-center">
+                            <div className="flex flex-col w-full">
+                                <label className="whitespace-nowrap w-auto">Email <sup className="text-red-500">*</sup></label>
+                                <input className="input mt-auto" />
+                            </div>
+                            <div className="flex flex-col w-full">
+                                <label className="whitespace-nowrap w-auto">Phone <sup className="text-red-500">*</sup></label>
+                                <input className="input mt-auto" />
+                            </div>
+                            <div className="flex flex-col w-full">
+                                <label className="whitespace-nowrap w-auto">Company</label>
+                                <input className="input mt-auto" />
+                            </div>
                         </div>
                     </div>
                 </div>
--- portfolio-project/src/app/page.tsx
+++ portfolio-project/src/app/page.tsx
@@ -6,6 +6,18 @@ export default function Home() {
       <div className="flex flex-row items-end justify-baseline pl-4 mt-4">
           <h1 className='h1 -mb-12.5'>HELLO.</h1>
       </div>
+      <div className="mt-5 flex items-center justify-center relative">
+        <img className='w-380'
+        src='/assets/bg.jpg'
+        alt='Capitol building of Madison, WI'
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center">
+          <h1 className="text-white text-8xl font-bold drop-shadow-lg">MICHAEL FISKEY</h1>
+          <h1 className="text-white text-8xl font-bold drop-shadow-lg">MICHAEL FISKEY</h1>
+          <h1 className="text-white text-8xl font-bold drop-shadow-lg">MICHAEL FISKEY</h1>
+        </div>
+      </div>
+
       <div className="mt-30 flex flex-row">
         <Image
           src='/assets/me.jpeg'
@@ -22,10 +34,6 @@ export default function Home() {
       <div className='flex flex-col justify-center m-10 items-center'>
         <h1 className="text-gray-800 text-[100px]">This is where I grew up...</h1>
       </div> 
-      <img className='relative w-full h-full object-cover'
-        src='/assets/madison_2.jpg'
-        alt='Capitol building of Madison, WI'
-      /> 
       </>
   );
 }
