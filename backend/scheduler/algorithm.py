81352d85c2a58f8a3ada52a1ecb51355fa1d740f
Michael Fiskey <fiskey.michael@gmail.com>
2025-07-31 15:21:15 -0500

    first optimal_schedules implementation (slow! needs logic updating!)

--- backend/scheduler/algorithm.py
+++ backend/scheduler/algorithm.py
@@ -2,8 +2,9 @@ from typing import Optional
 import pandas as pd
 from datetime import datetime, time, timedelta
 from when2meet_scraper import scrape
+pd.set_option('future.no_silent_downcasting', True)
 
-def get_max_duration(when2meet_schedule : pd.DataFrame):
+def get_max_duration(when2meet_schedule: pd.DataFrame) -> int:
     today = datetime.today().date()
     
     start_date = when2meet_schedule['date'].iloc[0].date()
@@ -30,22 +31,52 @@ def assign_song_part(name: str, song: str, part: str, when2meet_schedule: pd.Dat
     new_when2meet_schedule.to_csv('test1.csv')
     return new_when2meet_schedule
 
-def optimal_schedule(when2meet_schedule: pd.DataFrame = None, meeting_window=60):
+def optimal_schedules(when2meet_schedule: pd.DataFrame = None, meeting_window=60):
     if meeting_window < 15:
         raise ValueError(f'A meeting window must be at least 15 minutes!')
-    max = get_max_duration(when2meet_schedule)
-    if meeting_window > max:
-        raise ValueError(f'Meeting window has exceeded the schedule! Please choose a smaller meeting window less than {max} minutes.')
+    max_duration = get_max_duration(when2meet_schedule)
+    if meeting_window > max_duration:
+        raise ValueError(f'Meeting window has exceeded the schedule! Please choose a smaller meeting window less than {max_duration} minutes.')
     if meeting_window % 15 != 0:
         raise ValueError(f'A meeting window must be an increment of 15 minutes!')
     
-    # Convert meeting window to number of 15-minute slots
-    slots_needed = meeting_window // 15
-    return slots_needed
+    when2meet_schedule['available'] = True
+    pivot = when2meet_schedule.pivot_table(
+        index='date',
+        columns='name',
+        values='available',
+        fill_value=False,
+        aggfunc='any'
+    )
+
+    start, window_start = pivot.index.min(), pivot.index.min()
+    end = pivot.index.max()
+    date_range = pd.date_range(start=start, end=end, freq='15min')
+    empty_schedule = pd.DataFrame({'date': date_range})
+
+    schedule = empty_schedule.join(pivot, on='date', how='left').fillna(False).infer_objects(copy=False)
+    
+    schedule_tracker = {}
+
+    while (window_start < end) and (end - window_start) >= timedelta(minutes=meeting_window):
+        window_end = window_start + timedelta(minutes=meeting_window)
+        names = []
+        for name in schedule.columns[1:]:
+            if schedule.loc[(schedule['date'] >= window_start) & (schedule['date'] < window_end), name].all():
+                names.append(name)
+        if len(names) in schedule_tracker:
+            schedule_tracker[len(names)].append({'start': window_start, 'end': window_end, 'names': names})
+        else:
+            schedule_tracker[len(names)] = [{'start': window_start, 'end': window_end, 'names': names}]
+        window_start += timedelta(minutes=15)
+    if not schedule_tracker or max(schedule_tracker.keys()) == 0:
+        return 'NO SCHEDULES FOUND'
+    return schedule_tracker[max(schedule_tracker.keys())]
+
 
 def main():
-    #recording = scrape('https://www.when2meet.com/?27497701-jRmJk')
-    recording_small = scrape('https://www.when2meet.com/?31297823-f1ISk')
+    recording = scrape('https://www.when2meet.com/?27497701-jRmJk')
+    #recording_small = scrape('https://www.when2meet.com/?31297823-f1ISk')
 
     #svp = {'Judas' : {'T1' : ['gallop', 'jump'], 'T2': ['skip']}}
 
@@ -54,8 +85,8 @@ def main():
 
     #when2meet_schedule = assign_song_part('skip', 'judas', 't3', recording_small)
     #when2meet_schedule_2 = assign_song_part('gallop', 'blue', 't2', when2meet_schedule)
-    print(recording_small.head(30))
-    print(optimal_schedule(recording_small, 60))
+    print(recording.head(30))
+    print(optimal_schedules(recording, 15))
 
 if __name__ == '__main__':
     main()
