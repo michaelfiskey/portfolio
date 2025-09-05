98f52e9b15c71fc2773bc996b399f10d9f251215
Michael Fiskey <fiskey.michael@gmail.com>
2025-09-05 15:03:41 -0500

    updated email sending on conact page to be secure (trying to fix email time out issue)

--- express-backend/routes/contact.routes.js
+++ express-backend/routes/contact.routes.js
@@ -45,7 +45,9 @@ const handleValidationErrors = (req, res, next) => {
 
 const createTransport = () => {
     return nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE, 
+        host: 'smtp.gmail.com',
+        port: process.env.EMAIL_PORT,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS 
