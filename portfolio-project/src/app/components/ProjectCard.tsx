b4a1374eeae24c7ec64939d1106afe805e966c00
Michael Fiskey <fiskey.michael@gmail.com>
2025-09-11 16:08:33 -0500

    updated dev & data frontend on desktop (still need to do mobile)

--- portfolio-project/src/app/components/ProjectCard.tsx
+++ portfolio-project/src/app/components/ProjectCard.tsx
@@ -33,24 +33,49 @@ const ProjectCard = ({
 
     return (
         <Link href={`/dev-and-data/${projectPath}`}>
-            <div>
+            <div className="group relative">
                 <Card>
-                    <div className="flex flex-col lg:flex-row justify-between items-center bg-gradient-to-br from-stone-800 to-stone-700 border border-slate-200 rounded-2xl p-6 shadow-inner h-full w-full lg:max-h-[352px]">
-                        <img 
-                            src={`/assets/images/project-images/${projectImagePath}`}
-                            alt={projectTitle}
-                            className="w-full h-full max-w-[352px] max-h-[352px] object-cover rounded-lg"
+                    <div className="hidden md:block relative bg-gradient-to-br from-stone-800 to-stone-700 border border-slate-200 rounded-2xl overflow-hidden shadow-inner h-[300px] w-full">
+                        <div 
+                            className="absolute inset-0 bg-cover bg-center bg-no-repeat transition-transform duration-300 group-hover:scale-105"
+                            style={{
+                                backgroundImage: `url(/assets/images/project-images/${projectImagePath})`,
+                            }}
                         />
-                        <div className="mt-5 lg:mt-0 mb-4 flex flex-col justify-center items-center lg:w-1/2">
-                            <h3 className='h3 !text-white'>
-                                {projectTitle}
-                            </h3>
-                            <p className="text-white text-lg line-clamp-10 mb-5">
+                        <div className="absolute inset-0 bg-black/20 group-hover:bg-black/60 transition-all duration-300" />
+                            <div className="absolute inset-0 flex flex-col justify-center items-center p-6 opacity-0 group-hover:opacity-100 transition-all duration-300 transform translate-y-4 group-hover:translate-y-0">
+                            <h1 className='h3 !text-white text-center mb-4'>{projectTitle}</h1>
+                            <p className="text-white text-center text-lg line-clamp-4 mb-4">
                                 {projectDescription}
                             </p>
-                            <span className="bg-gradient-to-br border border-red-500 from-red-400 to-rose-300 text-stone-700 text-sm font-medium px-2 py-1 rounded-full">
+                            <span className="bg-gradient-to-br border border-red-500 from-red-400 to-rose-300 text-stone-700 text-sm font-medium px-3 py-2 rounded-full">
                                 <b>Created:</b> {formatDate(projectDate)}
                             </span>
+                        </div>
+                        <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/50 to-transparent p-4 group-hover:opacity-0 transition-opacity duration-300">
+                            <h3 className="h3 !text-white  text-lg">
+                                {projectTitle}
+                            </h3>
+                        </div>
+                    </div>
+                    <div className="block md:hidden">
+                        <div className="flex flex-col justify-between items-center bg-gradient-to-br from-stone-800 to-stone-700 border border-slate-200 rounded-2xl p-6 shadow-inner h-full w-full">
+                            <img 
+                                src={`/assets/images/project-images/${projectImagePath}`}
+                                alt={projectTitle}
+                                className="w-3/4 aspect-square object-cover object-center rounded-lg"
+                            />
+                            <div className="mt-5 mb-4 flex flex-col justify-center items-center">
+                                <h3 className='h3 !text-white text-center'>
+                                    {projectTitle}
+                                </h3>
+                                <p className="text-white text-center text-lg line-clamp-6 mb-5">
+                                    {projectDescription}
+                                </p>
+                                <span className="bg-gradient-to-br border border-red-500 from-red-400 to-rose-300 text-stone-700 text-sm font-medium px-2 py-1 rounded-full">
+                                    <b>Created:</b> {formatDate(projectDate)}
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </Card>
