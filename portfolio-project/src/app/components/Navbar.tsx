04d810b56c6f05da06e94eb678e853e0a3a0bec1
michaelfiskey <fiskey.michael@gmail.com>
2025-09-11 23:40:53 -0500

    updated navbar to now include social-media page and started embedding tiktoks

--- portfolio-project/src/app/components/Navbar.tsx
+++ portfolio-project/src/app/components/Navbar.tsx
@@ -26,14 +26,14 @@ const Navbar = () => {
 
     return (
         <div className='fixed bg-stone-700 shadow-xl top-0 w-full z-50 pt-3 pb-2 px-7'>
-            <div className='hidden md:grid md:grid-cols-3 px-15'>
+            <div className='hidden lg:grid lg:grid-cols-3 px-15'>
                 <p className='text-stone-100 justify-self-start font-[family-name:var(--font-bebas-neue)] text-2xl'>
                     {hours}
                     <span className={showColon ? "opacity-100 transition-opacity duration-200" : "opacity-0 transition-opacity duration-200"}>:</span>
                     {minutes}
                 </p>
                 <nav className='justify-self-end col-span-2'>
-                    <ul className='md:flex md:flex-row md:gap-5 lg:gap-10'>
+                    <ul className='lg:flex lg:flex-row lg:gap-10'>
                         <li className="group justify-self-center text-stone-100 hover:text-stone-400 transition-colors">
                             <Link href="/">home</Link>
                         </li>
@@ -47,6 +47,11 @@ const Navbar = () => {
                                 music <span className="inline-block group-hover:animate-bounce">&#9835;</span>
                             </Link>
                         </li>
+                        <li className="group text-stone-100 hover:text-stone-400 transition-colors">
+                            <Link href="/social-media" onClick={() => setIsMenuOpen(false)}>
+                                social media <span className="animate-spin inline-block">&copy;</span>
+                            </Link>
+                         </li>
                         <li className="justify-self-center text-stone-100 hover:text-stone-400 transition-colors">
                             <Link href="/contact">contact</Link>
                         </li>
@@ -76,7 +81,7 @@ const Navbar = () => {
                     </ul>
                 </nav>
             </div>
-            <div className='md:hidden flex justify-between items-center'>
+            <div className='lg:hidden flex justify-between items-center'>
                 <p className='text-stone-100 font-[family-name:var(--font-bebas-neue)] text-2xl'>
                     {hours}
                     <span className={showColon ? "opacity-100 transition-opacity duration-200" : "opacity-0 transition-opacity duration-200"}>:</span>
@@ -93,7 +98,7 @@ const Navbar = () => {
                     </div>
                 </button>
             </div>
-            <nav className={`md:hidden overflow-hidden transition-all duration-300 ${isMenuOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}>
+            <nav className={`lg:hidden overflow-hidden transition-all duration-300 ${isMenuOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}>
                 <ul className='pt-4 space-y-3 text-center'>
                     <li className="text-stone-100 hover:text-stone-400 transition-colors">
                         <Link href="/" onClick={() => setIsMenuOpen(false)}>home.</Link>
@@ -106,6 +111,11 @@ const Navbar = () => {
                     <li className="group text-stone-100 hover:text-stone-400 transition-colors">
                         <Link href="/music" onClick={() => setIsMenuOpen(false)}>
                             music <span className="inline-block group-hover:animate-bounce">&#9835;</span>
+                        </Link>
+                    </li>
+                    <li className="group text-stone-100 hover:text-stone-400 transition-colors">
+                        <Link href="/social-media" onClick={() => setIsMenuOpen(false)}>
+                            social media <span className="animate-spin inline-block">&copy;</span>
                         </Link>
                     </li>
                     <li className="text-stone-100 hover:text-stone-400 transition-colors">
